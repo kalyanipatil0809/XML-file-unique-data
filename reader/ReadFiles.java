0bf159834986ec137be827eeecc5afc72d62aee4
kalyanipatil0809 <98884029+kalyanipatil0809@users.noreply.github.com>
2022-02-25 16:58:16 +0530

    Add files via upload

--- reader/ReadFiles.java
+++ reader/ReadFiles.java
@@ -4,7 +4,9 @@ import java.util.Scanner;
 import org.apache.log4j.Logger;
 
 import in.sts.excelutility.files.ReadExcelFile;
+import in.sts.excelutility.files.ReadExcelTable;
 import in.sts.excelutility.files.ReadText;
+import in.sts.excelutility.files.ReadTextTable;
 import in.sts.excelutility.files.ReadXMLFile;
 
 public class ReadFiles {
@@ -16,6 +18,8 @@ public class ReadFiles {
 		System.out.println("Select 1 to read the Text file");
 		System.out.println("Select 2 to read the Excel file");
 		System.out.println("Select 3 to read the XML file");
+		System.out.println("select 4 for read table from Text file");
+		System.out.println("select 5 for read table from Excel file");
 
 		int select = selectcase.nextInt();
 
@@ -36,6 +40,19 @@ public class ReadFiles {
 		case 3:
 			ReadXMLFile readeXmlFile = new ReadXMLFile();
 			readeXmlFile.readXML();
+		
+
+			break;
+			
+		case 4:
+			ReadTextTable readTextTable = new ReadTextTable();
+			readTextTable.readTable();
+
+			break;
+			
+		case 5:
+			ReadExcelTable readExcelTable  = new ReadExcelTable();
+			readExcelTable.readExcel();
 
 			break;
 
